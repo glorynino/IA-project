92bc1205cea8d488daace238456080708e053fe5
glorynino <neil.isso10@gmail.com>
2024-11-13 21:28:49 +0100

    change

--- detection.py
+++ detection.py
@@ -1,6 +1,14 @@
 import cv2
-import mediapipe as pipe 
+import mediapipe as mp
 import time
 
 camera = cv2.VideoCapture(1)
+ptime = 0
+
+mpdraw = mp.solutions.drawing_utils
+mpfacemesh = mp.solutions.face_mesh
+
+
+facemesh = mpfacemesh.facemesh(maxface = 2)
+drawspec = mpdraw.Drawingspec(thickness = 2 , circle_radius =2)
 
